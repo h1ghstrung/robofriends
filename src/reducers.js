e29f20ad26c51920aa48cb97887daaa2d3121bf1
h1ghstrung <pydev4hire@gmail.com>
2018-09-17 16:39:44 -0400

    adapted for react-redux

---
+++ src/reducers.js
@@ -0,0 +1,16 @@
+export const CHANGE_SEARCH_FIELD = 'CHANGE_SEARCH_FIELD';
+
+const initialState = {
+	searchField: ''
+}
+
+
+//Reducer
+export const searchRobots = (state=initialState, action={}) => {
+	switch (action.type) {
+		case CHANGE_SEARCH_FIELD:
+			return Object.assign({}, state, { searchField:action.payload });
+		default:
+			return state;
+	}
+}
